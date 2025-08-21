d66ad169cd709ed43911f26843321690f13dc9b9
Kuan Wei Heng <kuanwh1996@gmail.com>
2025-08-21 14:59:32 +0000

    updates

--- task144.py
+++ task144.py
@@ -1,1 +1,6 @@
+p=lambda m:[[3>>c+d for c,d in zip(*r)]for r in zip(m,m[5:])]
+##
+p=lambda m:[[3>>c+d for c,d in zip(m.pop(5),r)]for r in m[:4]]
 p=lambda m:[[3-3*any(c)for c in zip(*r)]for r in zip(m,m[5:])]
+p=lambda m:m[5:]and[[3-3*any(c)for c in zip(*m[::5])]]+p(m[1:])
+p=lambda m:m[5:]and[[3-3*any(c)for c in zip(m.pop(0),m[4])]]+p(m)
--- task171.py
+++ task171.py
@@ -1,1 +1,6 @@
+p=lambda m:[[8,*[x]*(len(m[0])-2),8]for x in[8,*[0]*(len(m)-2),8]]
+
+##
+p=lambda m:[[8,*[x]*(len(m[0])-2),8]for x in[8,*[0]*(len(m)-2),8]]
 p=lambda m:(r:=[[8]*len(x:=m[0])])+[[8,*[0]*(len(x)-2),8]]*(len(m)-2)+r
+p=lambda m,i=3:-i*m or p([*map(list,zip(*([[8]*len(m[0])]+m[1:])[::-1]))],i-1)
