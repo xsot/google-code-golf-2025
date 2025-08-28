f59aa171daac88c27631fb60abf6b5d001c052d2
Oskar von Seeler <17097105+ovs-code@users.noreply.github.com>
2025-08-28 23:09:27 +0200

    solved 273, 275, 277

---
+++ merged/task273.py
@@ -0,0 +1,2 @@
+R=range(10)
+p=lambda g:[[g[i][j]|len({g[I][J]*(i>I,j>J)for I in{*R}-{i}for J in{*R}-{j}})//5*2for j in R]for i in R]
---
+++ merged/task275.py
@@ -0,0 +1,1 @@
+p=lambda g,k=-3:g*k or p([h:=[*zip(*g[(w:=len(g))::-1])],[[v*t//8for v in c for t in r]for c in h[:w]for r in h[w:]]]['8'in str(h[w:])],k+1)
