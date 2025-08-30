d6ec7c6995c8ffb84060d77b9fbbf860f5d6feef
Oskar von Seeler <17097105+ovs-code@users.noreply.github.com>
2025-08-31 00:47:05 +0200

    auto-merged

--- merged/task383.py
+++ merged/task383.py
@@ -3,3 +3,6 @@ p=lambda g,*G:[[r,[(o:=[*{}.fromkeys(r)]*3)[1+0**v]for v in r]][str(o)[1:8]in f'
 
 ### ovs (tied, 121 bytes)
 p=lambda g,*G:[[r,[(o:=[*{}.fromkeys(r)]*3)[1+0**v]for v in r]][str(o)[1:8]in f'{r+r[::-1]}']for r in zip(*G or p(g,*g))]
+
+## alternative with four rotations:
+p=lambda g,k=-3:g*k or p([[*[[P[v>0]for v in r]for*P,in zip([0,0]+r,[0]+r,r)if len({*P})==3>0==P[2]],r][0]for*r,in zip(*g[::-1])],k+1)
