598f711e0eb5327a7222dd19aef6add19fc4390c
Kuan Wei Heng <kuanwh1996@gmail.com>
2025-08-31 20:20:21 +0800

    auto-merged

--- merged/task366.py
+++ merged/task366.py
@@ -22,8 +22,8 @@ def p(g):
 def p(g):
  L=enumerate;M=sum(g,[]);*S,J,K,E=sorted({*M},key=M.count);F=[]
  if(Q:={K,E}<={*g[0]}):g=[*map(list,zip(*g))]
- for A,f in L(g):
-  for B,N in L(f):
+ for(A,f)in L(g):
+  for(B,N)in L(f):
    if E!=N!=K in f:
     C={(A,B)}
     for D in[*F]:
@@ -31,8 +31,8 @@ def p(g):
     F+=[C]
  for D in sorted(F,key=lambda s:-sum(g[A][B]!=J for(A,B)in s)):
   G,H=min(D)
-  for(I,f)in L(g):
+  for(A,f)in L(g):
    for(C,B)in L(f):
-    if E in f and all(len(g[0])>C+B-H>-1<I+f-G<len(g)and(J!=g[f][B]==g[f+I-G][B+C-H]!=E or J==g[f][B]and g[f+I-G][B+C-H]==E)for(f,B)in D):
-     for(O,P)in D:g[I+O-G][C+P-H]=g[O][P]
+    if E in f and all(len(g[0])>C+B-H>-1<A+f-G<len(g)and(J!=g[f][B]==g[f+A-G][B+C-H]!=E or J==g[f][B]and g[f+A-G][B+C-H]==E)for(f,B)in D):
+     for(O,P)in D:g[A+O-G][C+P-H]=g[O][P]
  g=[A for A in g if E in A];return[g,[*zip(*g)]][Q]
