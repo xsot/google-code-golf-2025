0d1e27420d3762d52a22e06cc6b36001a9088d4a
Oskar von Seeler <17097105+ovs-code@users.noreply.github.com>
2025-08-31 14:22:46 +0200

    auto-merged

--- merged/task080.py
+++ merged/task080.py
@@ -9,3 +9,16 @@ def p(g):
       for H,I in t+([-1,-1],[-1,1],[1,-1],[1,1]):
        if(R==y)*J(A+H,C+I)*J(K+H,L+I):c[K+H][L+I]=c[A+H][C+I]
     return[[y or c[A//E][C//E]for C,y in e(B)]for A,B in e(g)]
+
+### ovs (453 bytes)
+def p(g):
+ e=enumerate;t=6,11,10,7;E=[A+1for A,B in e(g)if len({*B})==1][0];c=[A[::E]for A in g[::E]];J=lambda A,B:len(c)>A>-1<B<len(c)
+ for A,B in e(c):
+  for C,y in e(B):
+   if sum(J(o:=A+P%3-1,z:=C+P%5-1)>0<y*c[o][z]for P in t)>3:
+    for K,B in e(c):
+     for L,R in e(B):
+      for P in t+(0,12,5,2):
+       H=P%3-1;I=P%5-1
+       if(R==y)*J(A+H,C+I)*J(K+H,L+I):c[K+H][L+I]=c[A+H][C+I]
+    return[[y or c[A//E][C//E]for C,y in e(B)]for A,B in e(g)]
--- merged/task366.py
+++ merged/task366.py
@@ -16,23 +16,3 @@ def p(g):
     if E in f and all(len(g[0])>B+N-H>-1<A+f-G<len(g)and(J!=g[f][N]==g[f+A-G][N+B-H]!=E or J==g[f][N]and g[f+A-G][N+B-H]==E)for(f,N)in D):
      for(O,P)in D:g[A+O-G][B+P-H]=g[O][P]
  g=[A for A in g if E in A];return[g,[*zip(*g)]][Q]
-
-### xsot (612 bytes)
-# combined (426 (612 unzipped) vs 374 bytes for gold)
-def p(g):
- L=enumerate;M=sum(g,[]);*S,J,K,E=sorted({*M},key=M.count);F=[]
- if(Q:={K,E}<={*g[0]}):g=[*map(list,zip(*g))]
- for(A,f)in L(g):
-  for(B,N)in L(f):
-   if E!=N!=K in f:
-    C={(A,B)}
-    for D in[*F]:
-     if{(A-1,B),(A,B-1)}&D:F.remove(D);C|=D
-    F+=[C]
- for D in sorted(F,key=lambda s:-sum(g[A][B]!=J for(A,B)in s)):
-  G,H=min(D)
-  for(A,f)in L(g):
-   for(B,N)in L(f):
-    if E in f and all(len(g[0])>B+N-H>-1<A+f-G<len(g)and(J!=g[f][N]==g[f+A-G][N+B-H]!=E or J==g[f][N]and g[f+A-G][N+B-H]==E)for(f,N)in D):
-     for(O,P)in D:g[A+O-G][B+P-H]=g[O][P]
- g=[A for A in g if E in A];return[g,[*zip(*g)]][Q]
