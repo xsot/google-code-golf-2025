6ae38c5f5cdf00b9149d42626934abbfecf3eb5f
Kuan Wei Heng <kuanwh1996@gmail.com>
2025-09-01 00:33:07 +0800

    add missing source

---
+++ xsot/task366.py
@@ -0,0 +1,13 @@
+def p(m):
+ a=sum(m,C:=[]);*_,B,S,D=sorted({*a},key=a.count)
+ if(S in m[0])*(D in m[0]):return[*zip(*p([*map(list,zip(*m))]))]
+ for(y,r)in enumerate(m):
+  for(x,v)in enumerate(r):
+   if D!=v!=S in r:c={(y,x)};[C.remove(d)or(c:=c|d)for d in(*C)if{(y-1,x),(y,x-1)}&d];C+=[c]
+# greedily start with the shape with the most dots
+ for(y,(G,H),*C)in sorted((-sum(B!=m[r][c]for(r,c)in C),min(C),*C)for(C)in C):
+  for(y,r)in enumerate(m):
+   for(x,v)in enumerate(r):
+# try to place this shape here: dots must match, and if one side has a dot the other must too
+    for(r,c)in C*all(len(m[0])>x-H+c>-1<y-G+r<len(m)and(B!=m[r][c]==m[y-G+r][x-H+c]or B==m[r][c]!=D==m[y-G+r][x-H+c])for(r,c)in C):m[y-G+r][x-H+c]=m[r][c]
+ return[r for(y,r)in enumerate(m)if D in r]
