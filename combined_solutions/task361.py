1ce0aa3239fe0da93e1e228367ba2a2ba11d9d40
Oskar von Seeler <17097105+ovs-code@users.noreply.github.com>
2025-08-31 13:08:30 +0200

    Saved 65b on: ['task361', 'task378']

--- combined_solutions/task361.py
+++ combined_solutions/task361.py
@@ -1,6 +1,1 @@
-def p(i,n=4,r=range(10)):
- for a in r:
-  for b in r:
-   i[b]+=0,;i+=i[0],
-   if all(all(x[b:b+n])for x in i[a:a+n]):return[[max(i[x][y],i[a+n+b+~y][b-a+x],i[a-b+y][b+n+a+~x],i[2*a+n+~x][2*b+n+~y])for y in r]for x in r]
- return p(i,n-1)
+def p(i,r=range(10)):I=[r*2for r in i*2];return[[[max(I[x][y],I[a+n+b+~y][b-a+x],I[a-b+y][b+n+a+~x],I[2*a+n+~x][2*b+n+~y])for y in r]for x in r]for n in(4,3,2)for a in r for b in r if all(all(x[b:b+n])for x in I[a:a+n])][0]
