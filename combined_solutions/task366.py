a0f74c0bc7783e24198276e79c8fad249497548f
Kuan Wei Heng <kuanwh1996@gmail.com>
2025-08-31 20:21:18 +0800

    Saved 0b on: ['task366']

--- combined_solutions/task366.py
+++ combined_solutions/task366.py
@@ -1,8 +1,8 @@
 def p(g):
  L=enumerate;M=sum(g,[]);*S,J,K,E=sorted({*M},key=M.count);F=[]
  if(Q:={K,E}<={*g[0]}):g=[*map(list,zip(*g))]
- for A,f in L(g):
-  for B,N in L(f):
+ for(A,f)in L(g):
+  for(B,N)in L(f):
    if E!=N!=K in f:
     C={(A,B)}
     for D in[*F]:
@@ -10,8 +10,8 @@ def p(g):
     F+=[C]
  for D in sorted(F,key=lambda s:-sum(g[A][B]!=J for(A,B)in s)):
   G,H=min(D)
-  for(I,A)in L(g):
-   for(C,B)in L(A):
-    if E in A and all(len(g[0])>C+B-H>-1<I+A-G<len(g)and(J!=g[A][B]==g[A+I-G][B+C-H]!=E or J==g[A][B]and g[A+I-G][B+C-H]==E)for(A,B)in D):
-     for(O,P)in D:g[I+O-G][C+P-H]=g[O][P]
+  for(A,f)in L(g):
+   for(B,N)in L(f):
+    if E in f and all(len(g[0])>B+N-H>-1<A+f-G<len(g)and(J!=g[f][N]==g[f+A-G][N+B-H]!=E or J==g[f][N]and g[f+A-G][N+B-H]==E)for(f,N)in D):
+     for(O,P)in D:g[A+O-G][B+P-H]=g[O][P]
  g=[A for A in g if E in A];return[g,[*zip(*g)]][Q]
