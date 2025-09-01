3da66980021fd34a86922613dc15409ca1682670
H <huntertr2002@gmail.com>
2025-09-01 00:32:35 -0500

    Saved 53b on: ['task005', 'task044', 'task101', 'task118', 'task158', 'task209', 'task238', 'task255', 'task268']

--- submission/task101.py
+++ submission/task101.py
@@ -1,11 +1,11 @@
-def p(g,R=range,l=len):
+def p(g):
  G=g
- for _ in' '*96:G=[r[::-1]for r in zip(*G[(0,)*l(G[0])in G[:min(y for y in R(l(G))if 1in G[y])]:])];h,w=l(G),l(G[0])
+ for _ in' '*96:G=[r[::-1]for r in zip(*G[(0,)*len(G[0])in G[:min(y for y in range(len(G))if 1in G[y])]:])];h,w=len(G),len(G[0])
  for s in[3,2,1]:
-  for y in R(15):
-   for x in R(-2,12):
-    m=[(H,W,G[H//s][W//s],l(g[0])>x+W>-1<y+H<l(g))for H in R(s*h)for W in R(s*w)]
-    if all((2^u or V)*(1>V or(g[y+H][x+W]==2)==(u==2))for H,W,u,V in m):
+  for y in range(15):
+   for x in range(-2,12):
+    m=[(H,W,G[H//s][W//s],len(g[0])>x+W>-1<y+H<len(g))for H in range(s*h)for W in range(s*w)]
+    if all((2^u or V)*(V<1or(g[y+H][x+W]==2)==(u==2))for H,W,u,V in m):
      for H,W,u,V in m:
       if V:g[y+H][x+W]=u+u//2
  return eval(str(g).replace(*'32'))
--- submission/task158.py
+++ submission/task158.py
@@ -1,11 +1,10 @@
 def p(g):
- o=g[0][-1];e=enumerate;_,t=max((len({*sum(a:=[R[x:x+3]for R in g[y:y+3]],[])}),a)for y,r in e(g)for x,c in e(r))
- for s in[1,2,3]:
-  for y,r in e(g[:1-3*s]):
-   for x,c in e(r[:1-3*s]):
+ o=g[0][-1];s,t=max((len({*sum(a:=[R[x:x+3]for R in g[y:y+3]],[])}),a)for y in range(len(g))for x in range(len(g[0])))
+ for s in(1,2,3):
+  for y in range(len(g)+1-3*s):
+   for x in range(len(g[0])+1-3*s):
     for z in'range'*2:
-     t=[t,[*zip(*t)]][z<'r'][::-1];R=range(s*3)
-     if all((a:=g[y+Y][x+X])==(b:=t[Y//s][X//s])or(a==o)*(b==max({*t[1]}-{o}))for Y in R for X in R):
-      for Y in R:
-       for X in R:g[y+Y][x+X]=t[Y//s][X//s]
+     t=(t,[*zip(*t)])[z<'r'][::-1]
+     for Y in range(s*3*all((a:=g[y+Y][x+X])==(b:=t[Y//s][X//s])or(a==o)*(b==max({*t[1]}-{o}))for Y in range(s*3) for X in range(s*3))):
+      for X in range(s*3):g[y+Y][x+X]=t[Y//s][X//s]
  return g
