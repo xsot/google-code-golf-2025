22282c230757db8435918ed34d79e64cf797a7a9
Jo King <653testing@gmail.com>
2025-09-01 16:25:35 +1000

    Saved 89b on: ['task012', 'task034', 'task040', 'task319']

--- submission/task012.py
+++ submission/task012.py
@@ -1,2 +1,1 @@
-E=enumerate
-p=lambda g:[[v or max({*zip(b'\0',[v]+sorted(f:=sum(g,[]),key=f.count))}&{((I//12-i)**2+(I%12-j)**2,V)for I,V in E(f)})[1]for j,v in E(r)]for i,r in E(g)]
+import re;p=lambda i,k=7:-k*i or p(eval(re.sub("(([^0]).{37}([^0]), )0(, 0.{31})0, 0,",r"\1\2\4\3,0,\2+",str([*zip(*i[::-1])]))),k-1)
