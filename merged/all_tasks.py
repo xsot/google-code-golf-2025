5dc7d4e65324abf8ecc864c561b1e461e08be4bb
Oskar von Seeler <17097105+ovs-code@users.noreply.github.com>
2025-09-01 23:03:14 +0200

    auto-merged

--- merged/all_tasks.py
+++ merged/all_tasks.py
@@ -126,16 +126,13 @@ p=lambda i,k=3:-k*i or[(l:=0)or[y+0*(l:=l|(1>a<y!=b)*b)or l for a,b,y in zip([0,
 p=lambda a:[3*[1//len({*b})*5]for b in a]
 # task 53: 21 bytes, gold, https://arcprize.org/play?task=25ff71a9
 p=lambda a:(a+a)[2:5]
-# task 54: 381 (589 unzipped) vs 356 bytes for gold, https://arcprize.org/play?task=264363fd
+# task 54: 314 (379 unzipped) vs 356 bytes for gold, https://arcprize.org/play?task=264363fd
 def p(g):
- B=enumerate;K=sum(g,[]);*R,C,A=sorted({*K},key=K.count);L=max(G for(E,D)in B(g)for(F,G)in B(D[:-1])if G!=A!=C==D[F+1]);I,J=max([E,F]for(E,D)in B(g)for(F,G)in B(D[:-1])if g[E][F]==L!=A!=D[F+1]!=C!=D[F-1]!=A)
- for(F,G)in[[E,F]for(E,D)in B(g)for(F,G)in B(D[:-1])if G==L!=C==D[F+1]]:
-  for Q in range(9):
-   if g[I-1+Q//3][J-1+Q%3]-A:g[F-1+Q//3][G+1-Q%3]=g[I-1+Q//3][J-1+Q%3]
-  for(N,O)in[[-1,0],[1,0],[0,1],[0,-1]]:
-   D,E=N*2,O*2;P=g[I+D][J+E]
-   while C!=P!=A<len(g[0])>G+E>-1<F+D<len(g)>g[F+D][G+E]!=A:g[F+D][G+E]=P;D+=N;E+=O
- for Q in range(25):g[I-2+Q//5][J-2+Q%5]=A
+ f=sum(g,[]);*C,_,B=sorted({*f},key=f.count);M={(i,j):v for i,r in enumerate(g)for j,v in enumerate(r)if v in C};T={(i,j)for i,j in M for I,J in M if(I-i)**2+(J-j)**2==1}
+ for i,j in{*M}-T:
+  for I,J in T:
+   y,x=zip(*T);d=I-sum(y)//len(y);D=J-sum(x)//len(x);g[I][J]=B;F=1
+   while((r:=g[d+i])[D+j]^B)*F:r[D+j]=M[I,J];d+=(d>0)-(d<0);D+=(D>0)-(D<0);F-=-3<d|D<3
  return g
 # task 55: 104 vs 84 bytes for gold, https://arcprize.org/play?task=272f95fa
 p=lambda i,z=0:[[z:=z+1-((c:=0)in x)]and[-c+(c:=c+y)or[0,2,0,4,6,3,0,1,0][z*3+c%7]for y in x]for x in i]
