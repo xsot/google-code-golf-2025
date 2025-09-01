d335f25d9815045012b8b865c3c512b31376e50f
Oskar von Seeler <17097105+ovs-code@users.noreply.github.com>
2025-09-01 17:09:26 +0200

    Saved 13b on: ['task319']

--- submission/task319.py
+++ submission/task319.py
@@ -1,3 +1,1 @@
-q=0
-for n in b'@@@%@   \n@),a0%!  @$E`	sD 1':q=q*128+n
-p=lambda i:[[[t[-1],y][y==l]for*s,y in zip(*i,x)if l in s]for x in i if(l:=(t:=sorted({*(z:=sum(i,[]))},key=z.count))[1&q>>hash((*z,))%3999%2529%1577%989%616])in x]
+p=lambda i:[[[t[3],y][y==l]for*s,y in zip(*i,x)if l in s]for x in i if(l:=(t:=sorted({*(z:=sum(i,[]))},key=z.count))[1&b'1 Ds	`E$@  !%0a,)@\n   @%@@@'[(m:=hash((*z,))%3999%2529%1577%989%616)//7]>>m%7])in x]
