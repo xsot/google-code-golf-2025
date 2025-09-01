020b58eae21149a84efc3c5b6e56ff97ff96afba
H <huntertr2002@gmail.com>
2025-09-01 17:02:35 -0500

    auto-merged

--- merged/task054.py
+++ merged/task054.py
@@ -1,13 +1,12 @@
-# ovs (314 (379 unzipped) bytes, gold)
+# mwi (308 (379 unzipped) vs 314 bytes for gold)
 def p(g):
- f=sum(g,[]);*C,_,B=sorted({*f},key=f.count);M={(i,j):v for i,r in enumerate(g)for j,v in enumerate(r)if v in C};T={(i,j)for i,j in M for I,J in M if(I-i)**2+(J-j)**2==1}
- for i,j in{*M}-T:
-  for I,J in T:
+ f=sum(g,[]);*C,p,B=sorted({*f},key=f.count);M={(i,I):J for(i,j)in enumerate(g)for(I,J)in enumerate(j)if J in C};T={(i,j)for(i,j)in M for(I,J)in M if(I-i)**2+(J-j)**2==1}
+ for(i,j)in{*M}-T:
+  for(I,J)in T:
    y,x=zip(*T);d=I-sum(y)//len(y);D=J-sum(x)//len(x);g[I][J]=B;F=1
    while((r:=g[d+i])[D+j]^B)*F:r[D+j]=M[I,J];d+=(d>0)-(d<0);D+=(D>0)-(D<0);F-=-3<d|D<3
  return g
-
-### mwi (368 (593 unzipped) bytes)
+## alt
 def p(g):
  B=enumerate;K=sum(g,[]);*R,C,A=sorted({*K},key=K.count);L=max(G for(E,D)in B(g)for(F,G)in B(D[:-1])if G!=A!=C==D[F+1]);I,J=max([E,F]for(E,D)in B(g)for(F,G)in B(D[:-1])if g[E][F]==L!=A!=D[F+1]!=C!=D[F-1]!=A)
  for(F,G)in[[E,F]for(E,D)in B(g)for(F,G)in B(D[:-1])if G==L!=C==D[F+1]]:
@@ -17,6 +16,15 @@ def p(g):
    N,O=Q%3-1,Q//3-1;D,E=N*2,O*2;P=g[I+D][J+E]
    while(N+O)*(C!=P!=A<len(g[0])>G+E>-1<F+D<len(g)>g[F+D][G+E]!=A):g[F+D][G+E]=P;D+=N;E+=O
  for Q in range(25):g[I-2+Q//5][J-2+Q%5]=A
+ return g
+
+### ovs (314 (379 unzipped) bytes)
+def p(g):
+ f=sum(g,[]);*C,_,B=sorted({*f},key=f.count);M={(i,j):v for i,r in enumerate(g)for j,v in enumerate(r)if v in C};T={(i,j)for i,j in M for I,J in M if(I-i)**2+(J-j)**2==1}
+ for i,j in{*M}-T:
+  for I,J in T:
+   y,x=zip(*T);d=I-sum(y)//len(y);D=J-sum(x)//len(x);g[I][J]=B;F=1
+   while((r:=g[d+i])[D+j]^B)*F:r[D+j]=M[I,J];d+=(d>0)-(d<0);D+=(D>0)-(D<0);F-=-3<d|D<3
  return g
 
 ### combined (381 (589 unzipped) bytes)
