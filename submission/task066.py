31e67044a4bb2c86abab35e3778251103d58b4e8
Oskar von Seeler <17097105+ovs-code@users.noreply.github.com>
2025-09-02 00:33:09 +0200

    Saved 23b on: ['ovs/task066.py', 'submission/task066.py']

--- submission/task066.py
+++ submission/task066.py
@@ -1,12 +1,5 @@
 def p(g):
- def p(y,x,A,B,V,g,C=0,G=0):
-  if(len(g)>x>-1<y<len(g))*16<=C+13|G:return
-  if(t:=g[y][x])==2:
-   for i,j in V:g[i][j]=3
-   return
-  if t>7:
-   for D,E in[[B,A],[-B,A],[B,-A],[-B,-A]]*G:p(y-A+D,x-B+E,D,E,{*V,(y-A,x-B)},g,C+1)
-   return
-  p(y+A,x+B,A,B,{*V,(y,x)},g,C,G+1)
- for(A,B),(C,D)in(X:=[(A,B)for A,C in enumerate(g)for B,D in enumerate(C)if g[A][B]==3]),X[::-1]:p(C+C-A,D+D-B,C-A,D-B,X,g)
+ M={A*1j+B:D for A,C in enumerate(g)for B,D in enumerate(C)};A,C=[y for y in M if M[y]==3];S=[(C+C-A,C-A,0,0),(A+A-C,A-C,0,0)]
+ for*V,y,A,C,G in S:
+  if(y in M)*16>G|C+13>1>(M[y]==2!=exec("for y in V:g[int(y.imag)][int(y.real)]=3")):F=M[y]>7;y-=A*F;S+=[(y,*V,y+D,D,C+F,~-F*~G)for D in[A][F:]or[A*1j,A/1j]*G]
  return g
