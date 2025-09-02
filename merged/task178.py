85d659667cfb8e236c1ab75099ce4a8da899e0bd
Kuan Wei Heng <kuanwh1996@gmail.com>
2025-09-02 20:48:42 +0800

    auto-merged

--- merged/task178.py
+++ merged/task178.py
@@ -4,9 +4,9 @@ p=lambda a:a*-1*-1or[p(b)for b in a if a!=(a:=b)]
 ### combined (tied, 49 bytes)
 p=lambda a:a*-1*-1or[p(b)for b in a if a!=(a:=b)]
 
-### xsot (57 bytes)
+### xsot (55 bytes)
 # ovs + att
-p=lambda g,*a:[y for*y,in zip(*a or p(g,*g))if g!=(g:=y)]
+p=lambda g,*a:[y for*y,in zip(*a or p(*g))if g!=(g:=y)]
 
 ##
 p=lambda m,i=1:-i*m or p([*zip(*m[:1]+[y for x,y in zip(m,m[1:])if x!=y])],i-1)
