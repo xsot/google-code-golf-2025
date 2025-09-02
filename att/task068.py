afae7aa26c40a97bec949a510fbb20c3a61ffcf9
zacjl <17119049+zacjl@users.noreply.github.com>
2025-09-01 17:32:06 -0700

    Saved 3b on: ['att/task068.py', 'submission/task068.py']

--- att/task068.py
+++ att/task068.py
@@ -1,2 +1,2 @@
-z=[[0]*10]
-p=lambda a:eval(f'[[[0,2,2,*a[1]][[*map("{a}".count,str(a))].count(1)]'+'for*a,in map(zip,z+a,a,a,a[1:]+z)]'*2)
+s='for*a,in map(zip,[s]+a,a,a,a[1:]+[s])]'
+p=lambda a:eval(f'[[[0,2,2,*a[1]][[*map("{a}".count,str(a))].count(1)]'+s*2)
