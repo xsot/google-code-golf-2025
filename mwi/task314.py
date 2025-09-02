05c098f6a1256d259d9a629225f6f0a01fb42890
H <huntertr2002@gmail.com>
2025-09-01 21:50:48 -0500

    Saved 23b on: ['mwi/task143.py', 'mwi/task314.py', 'mwi/task361.py', 'mwi/task387.py', 'mwi/task396.py', 'submission/task143.py', 'submission/task314.py', 'submission/task361.py', 'submission/task387.py', 'submission/task396.py']

---
+++ mwi/task314.py
@@ -0,0 +1,4 @@
+p=lambda i,r=range(8):[[max((x:=i[a])[b-5]&x[b-3],i[a-5][b]&i[a-3][b],x[b])for b in r]for a in r]
+##
+p=lambda i,e=enumerate:[[max(y[b-5]&y[b-3],i[a-5][b]&i[a-3][b],x)for b,x in e(y)]for a,y in e(i)]
+p=lambda i,r=range(8):[[((x:=i[a])[b-5]>1)*x[b-3]|(i[a-5][b]>1)*i[a-3][b]or x[b]for b in r]for a in r]
--- submission/task314.py
+++ submission/task314.py
@@ -1,1 +1,1 @@
-p=lambda i,r=range(8):[[((x:=i[a])[b-5]>1)*x[b-3]|(i[a-5][b]>1)*i[a-3][b]or x[b]for b in r]for a in r]
+p=lambda i,r=range(8):[[max((x:=i[a])[b-5]&x[b-3],i[a-5][b]&i[a-3][b],x[b])for b in r]for a in r]
