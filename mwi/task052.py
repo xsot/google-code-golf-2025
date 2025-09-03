94b240593c279a553111c3645d1bbdc6ca82eeaa
H <huntertr2002@gmail.com>
2025-09-03 02:42:02 -0500

    52 alts

--- mwi/task052.py
+++ mwi/task052.py
@@ -1,3 +1,10 @@
 p=lambda a:[3*[5*(b==b[:1]*3)]for b in a]
 
 ##
+
+p=lambda a:[3*[1//len({*b})*5]for b in a]
+p=lambda a:[3*[5*(a==[b]*2)]for*a,b in a]
+p=lambda a:[3*[5*(x==y==z)]for x,y,z in a]
+p=lambda a:[3*[len({*b})%3%2*5]for b in a]
+p=lambda a:[3*[5*({*a}=={b})]for*a,b in a]
+p=lambda a:[3*[5*(sorted(a)==a)]for a in a]
