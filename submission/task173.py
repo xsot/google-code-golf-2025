f1023648fbd20a8e90aef8d7f81a92e54707c5c5
Oskar von Seeler <17097105+ovs-code@users.noreply.github.com>
2025-09-03 20:23:18 +0200

    Saved 30b on: ['ovs/task173.py', 'ovs/task191.py', 'submission/task173.py', 'submission/task191.py']

--- submission/task173.py
+++ submission/task173.py
@@ -1,6 +1,1 @@
-def p(g):
- e=enumerate;o=[(s,y,x)for y,r in e(g)for x,c in e(r)if any(s:=sum([r[x:x+3]for r in g[y:y+3]],[]))*s==s[::-1]and s[8:]]
- for a,y,x in o:
-  for A,Y,X in o:
-   for y,r in e(a*(len({*a})==3*any(all(a[y]in[r,V]for y,r in e(A))for V in{*a}))):g[Y+y//3][X+y%3]=r
- return g
+def p(g):o=[(Y,x,y)for Y,a in enumerate(g)for x,a in enumerate(a)if(y:=[a for a in g[Y:Y+3]for a in a[x:x+3]])==y[::-1]>y[:8]>[0]*8];[0for Y,X,a in o for Y,X,A in o for y,g[Y+y//3][X+y%3]in enumerate(a*(len({*a})==3*len({a[y]for y,r in enumerate(A)if r-a[y]})))];return g
