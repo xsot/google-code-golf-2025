a2a5193b83067b8ae384f4da0437c99751d39b84
H <huntertr2002@gmail.com>
2025-09-03 02:44:46 -0500

    auto-merged

--- merged/task052.py
+++ merged/task052.py
@@ -6,6 +6,13 @@ p=lambda a:[3*[5*(b==b[:1]*3)]for b in a]
 
 ##
 
+p=lambda a:[3*[1//len({*b})*5]for b in a]
+p=lambda a:[3*[5*(a==[b]*2)]for*a,b in a]
+p=lambda a:[3*[5*(x==y==z)]for x,y,z in a]
+p=lambda a:[3*[len({*b})%3%2*5]for b in a]
+p=lambda a:[3*[5*({*a}=={b})]for*a,b in a]
+p=lambda a:[3*[5*(sorted(a)==a)]for a in a]
+
 ### ovs (tied, 41 bytes)
 p=lambda g:[3*[1//len({*r})*5]for r in g]
 
