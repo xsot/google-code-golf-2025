8f0806d34439cf9363b2180056c0a5fb67d19528
Oskar von Seeler <17097105+ovs-code@users.noreply.github.com>
2025-09-04 18:11:17 +0200

    Saved 21b on: ['ovs/task390.py', 'ovs/task392.py', 'ovs/task396.py', 'submission/task390.py', 'submission/task392.py', 'submission/task396.py']

--- submission/task390.py
+++ submission/task390.py
@@ -1,1 +1,1 @@
-p=lambda m,k=3,R=range(-14,1):-k*m or p([*zip(*[m[(len([l:=i-j for j in R if 2in m[-j]])==4>abs(l)>1)*2*l-i]for i in R])],k-1)
+p=lambda g,k=-1:k*g or p([*zip(*[[*[g[k-~k]for k in(1,2,-3,-4)if g[k].count(2)>4],r][0]for r in g if(g:=g[1:]+[r])])],k+1)
--- submission/task392.py
+++ submission/task392.py
@@ -1,4 +1,1 @@
-def p(g):
- e=enumerate;C=max(sum(g,[]));o=2+(f"{C}, 0, 0, {C}"in str(g))
- for k,_ in e(g):g=[[c or any(any(s[x-(x>0):x+2])for s in g[y-(y>0):y+2])*[C,5][k%o<o-1]for x,c in e(r)]for y,r in e(g)]
- return g
+def p(g):e=enumerate;C=max(sum(g,[]));[g:=[[c or _*any(any(s[x+x%~x:x+2])for s in g[y+y%~y:y+2])for x,c in e(r)]for y,r in e(g)]for _ in[5,*[5]*(f"{C}, 0, 0, {C}"in str(g)),C]*3];return g
