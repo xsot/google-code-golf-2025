cd6ca24096bd9bcb8316c5d14508dd7af73f875a
Oskar von Seeler <17097105+ovs-code@users.noreply.github.com>
2025-09-05 15:33:56 +0200

    +15=956199/959003

--- merged/task345.py
+++ merged/task345.py
@@ -1,2 +1,5 @@
-# combined (109 vs 90 bytes for gold)
+# ovs (108 vs 90 bytes for gold)
+p=lambda i,r=range(10):[[i[a][b]|2&6%~sum((t:=[*zip(*i)])[b-1][a+a%~a:])+max(t[b][a:])for b in r]for a in r]
+
+### combined (109 bytes)
 p=lambda i,r=range(10):[[i[a][b]|2&6%~sum((t:=[*zip(*i)])[b-1][a-(a>0):])+max(t[b][a:])for b in r]for a in r]
---
+++ ovs/task097.py
@@ -0,0 +1,1 @@
+p=lambda i,e=enumerate:[[y*(sum(sum([z[b+b%~b:b+2]for z in i[a+a%~a:a+2]],[]))>y)for b,y in e(x)]for a,x in e(i)]
