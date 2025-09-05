4e224fa56d32b2fed88721522be6b572fca0f821
zacjl <17119049+zacjl@users.noreply.github.com>
2025-09-04 20:26:54 -0700

    Saved 2b on: ['att/task228.py', 'submission/task228.py']

--- att/task228.py
+++ att/task228.py
@@ -1,2 +1,2 @@
 import re
-p=lambda a,n=-3:n*a or[*zip(*eval(re.sub(r'([^0]), (?!\1|0)(.)(.*\1.{34})0',r'\1,0\3\2',str(p(a,n+1)[::-1]))))]
+p=lambda a,n=-3:n*a or[*zip(*eval(re.sub(r'([^0])((, (?!\1|0).).*0\3.{28})0',r'0\2\1',str(p(a,n+1)[::-1]))))]
