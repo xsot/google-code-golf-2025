fdb35a6e573ae2ad64305fe86865b5f09f987718
H <huntertr2002@gmail.com>
2025-09-11 04:19:47 -0500

    task099 alt

--- mwi/task099.py
+++ mwi/task099.py
@@ -1,1 +1,4 @@
-p=lambda g,r=range(10):[[g[y][x]or max(sum(g[y:y+4],g[y-1]))*any(g[y-9][:x+1]*any(g[y-9][x:]))for x in r]for y in r]
+p=lambda g,r=range(10):[[g[y][x]or max(sum(g[y:y+4],g[y-1]))*any(g[y-9][:x+1])*any(g[y-9][x:])for x in r]for y in r]
+
+##
+p=lambda g,r=range(10):[[g[y][x]or max(sum(g[y//5*5:][:5],[]))*any(g[y-9][:x+1])*any(g[y-9][x:])for x in r]for y in r]
