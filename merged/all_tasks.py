aa2e7c9042acb496be121c476de7406c4422cd74
ovs <17097105+ovs-code@users.noreply.github.com>
2025-09-19 22:40:55 +0200

    +10=959097/960259

--- merged/all_tasks.py
+++ merged/all_tasks.py
@@ -1010,8 +1010,8 @@ p=lambda i:[[x[:2*(n:=122%len(i))][b-n]for b,y in enumerate(x)if y<1]for x in i 
 p=lambda a,n=[]:a*0!=0and[*map(p,a,a[3:]+n)]or~a+~n&2
 # task 396: 156 bytes, gold, https://arcprize.org/play?task=fcb5c309
 p=lambda m,X=8,f=0:[[sum({*e*sum(m,[-f])})for e in r[x:x+X]]for x in range(len(m[0]))for r in m+[[0]*99]if(f:=r[x]*(X*[r[x]]in(r[x:x+X],[f]*X)))]or p(m,X-1)
-# task 397: 125 vs 121 bytes for gold, https://arcprize.org/play?task=fcc82909
-p=lambda m,k=0:exec('r=k//9;exec("m[2%s=3,3;r+=1;"*len(a:={*m[%s+m[1%s})*(not{0,3}&a));k+=1;'%(('+r][k%9:k%9+2]',)*3)*81)or m
+# task 397: 115 bytes, gold, https://arcprize.org/play?task=fcc82909
+p=lambda m,k=80:exec('r=k//9-1;exec("%s=3,3;"*len(a:={*%s+%s})*all(a));k-=1;'%(('m[r:=r+1][k%9:k%9+2]',)*3)*81)or m
 # task 398: 79 vs 77 bytes for gold, https://arcprize.org/play?task=feca6190
 def p(a):b,=a;l=25-5*b.count(0);return[((~-l*[0]+b)*2)[i:i+l]for i in range(l)]
 # task 399: 64 bytes, gold, https://arcprize.org/play?task=ff28f65a
--- ovs/task397.py
+++ ovs/task397.py
@@ -1,1 +1,1 @@
-p=lambda m,k=0:exec('r=k//9;exec("m[2%s=3,3;r+=1;"*len(a:={*m[%s+m[1%s})*(not{0,3}&a));k+=1;'%(('+r][k%9:k%9+2]',)*3)*81)or m
+p=lambda m,k=80:exec('r=k//9-1;exec("%s=3,3;"*len(a:={*%s+%s})*all(a));k-=1;'%(('m[r:=r+1][k%9:k%9+2]',)*3)*81)or m
