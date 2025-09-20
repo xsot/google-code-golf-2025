349a5f5c7c33380fd97e6bd6407040d684c743f4
H <huntertr2002@gmail.com>
2025-09-19 21:20:20 -0500

    +5=959140/960281

--- merged/task363.py
+++ merged/task363.py
@@ -1,4 +1,11 @@
-# mwi (210 bytes, gold)
+# mwi (205 bytes, gold)
+def p(g):
+ v=(f:=lambda C:{x+x//10*80for x in range(100)if C==g[x//10][x%10]})(2)
+ for y in f(0)|f(5):
+  for Y in(m:=[Y+y-min(v)for Y in v])*(hash((*g[0],))%263+y!=99!={*m}<f(0)):g[Y//90][Y%90]=2
+ return g
+
+## no lambda
 def p(g):
  z,v,V=[{x+x//10*80for x in range(100)if C==g[x//10][x%10]}for C in(0,2,5)]
  for y in z|V:
