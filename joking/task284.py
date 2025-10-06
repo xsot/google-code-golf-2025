ffcd752951488ee0bd3352c3d619e92b339cc045
Jo King <653testing@gmail.com>
2025-10-07 04:17:13 +1100

    +7=960305/961182 109💎 242🥇 73🥈 55🥉

--- joking/task284.py
+++ joking/task284.py
@@ -1,5 +1,3 @@
-# joking (221 (529 unzipped) vs 201 bytes for gold)
-# 222
 def p(i):i=[*zip(*i)];[i:=[[([*(l[1]-l[0]-2)//2*[1],7,4,4,7,*(l[1]-l[0]-2)//2*[1],*7*[0]][l[1]-b]>>abs(a)&1)*max(i)[l[l[1]-b<b-l[0]]]for b,x in enumerate(x)]for a,x in enumerate(i,-a)]for a,x in enumerate(i)if max(i)==x!=len(l:=[b for b,x in enumerate(x)if x])==2];i=[*zip(*i)];[i:=[[([*(l[1]-l[0]-2)//2*[1],7,4,4,7,*(l[1]-l[0]-2)//2*[1],*7*[0]][l[1]-b]>>abs(a)&1)*max(i)[l[l[1]-b<b-l[0]]]for b,x in enumerate(x)]for a,x in enumerate(i,-a)]for a,x in enumerate(i)if max(i)==x!=len(l:=[b for b,x in enumerate(x)if x])==2];return i
 
 ##
@@ -12,16 +10,3 @@ def p(i):
   *i,=zip(*i);m=max(i)
   if~len(l:=[n for n,y in E(m)if y])&1:s,t=l;i=[[([*(t-s-2)//2*[1],7,4,*[0]*4][min(t-b,b-s)]>>abs(a-i.index(m))&1)*m[l[t-b<b-s]]for b,_ in E(x)]for a,x in E(i)]
  return i
-
-### ovs (228 bytes)
-def p(i):
- for E in[enumerate]*2:
-  *i,=zip(*i);m=max(i)
-  if len(l:=[n for n,y in E(m)if y])==2:s,t=l;i=[[([*(t-s-2)//2*[1],7,4,*[0]*4][min(t-b,b-s)]>>abs(a-i.index(m))&1)*m[l[t-b<b-s]]for b,_ in E(x)]for a,x in E(i)]
- return i
-
-### combined (238 bytes)
-def p(i,k=1,E=enumerate):
- m=max(i)
- if len(l:=[n for n,y in E(m)if y])==2:s,t=l;i=[[(abs(a-i.index(m))in[*(t-s-2)//2*[[0]],[0,1,2],[2],*[[]]*9][min(t-b,b-s)])*m[l[t-b<b-s]]for b,_ in E(x)]for a,x in E(i)]
- return-k*i or p([*zip(*i)],k-1)
--- merged/all_tasks.py
+++ merged/all_tasks.py
@@ -704,12 +704,8 @@ p=lambda i,k=39:-k*i or p(eval(re.sub("(\((?=[^)]+[1-9])[^)]+., )(\([^)]+.), \((
 p=lambda i,*x,c=0:[c|(c:=i)>>2|(i:=y)for y in x+x[7:]]or[*map(p,*map(p,*i))]
 # 💎 task 283: 81 bytes, gold, https://arcprize.org/play?task=b6afb2da
 p=lambda i,r=[[0]*25]*25,*w:r and[*map(p,i,r,r[:1]+i,i[1:]+r,*w)]or sum(w)%8*i//8
-# ❌ task 284: 228 vs 201 bytes for gold, https://arcprize.org/play?task=b7249182
-def p(i):
- for E in[enumerate]*2:
-  *i,=zip(*i);m=max(i)
-  if len(l:=[n for n,y in E(m)if y])==2:s,t=l;i=[[([*(t-s-2)//2*[1],7,4,*[0]*4][min(t-b,b-s)]>>abs(a-i.index(m))&1)*m[l[t-b<b-s]]for b,_ in E(x)]for a,x in E(i)]
- return i
+# 🥉 task 284: 221 (529 unzipped) vs 201 bytes for gold, https://arcprize.org/play?task=b7249182
+def p(i):i=[*zip(*i)];[i:=[[([*(l[1]-l[0]-2)//2*[1],7,4,4,7,*(l[1]-l[0]-2)//2*[1],*7*[0]][l[1]-b]>>abs(a)&1)*max(i)[l[l[1]-b<b-l[0]]]for b,x in enumerate(x)]for a,x in enumerate(i,-a)]for a,x in enumerate(i)if max(i)==x!=len(l:=[b for b,x in enumerate(x)if x])==2];i=[*zip(*i)];[i:=[[([*(l[1]-l[0]-2)//2*[1],7,4,4,7,*(l[1]-l[0]-2)//2*[1],*7*[0]][l[1]-b]>>abs(a)&1)*max(i)[l[l[1]-b<b-l[0]]]for b,x in enumerate(x)]for a,x in enumerate(i,-a)]for a,x in enumerate(i)if max(i)==x!=len(l:=[b for b,x in enumerate(x)if x])==2];return i
 # 💎 task 285: 275 (431 unzipped) bytes, gold, https://arcprize.org/play?task=b775ac94
 def	p(g):
 	for*I,in[[]]*8:
