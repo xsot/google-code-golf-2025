25e387f1f2fcbd8f525fdeac8a60b8cba936fb92
H <huntertr2002@gmail.com>
2025-10-19 15:25:39 -0500

    +0=961211/961728 155💎 298🥇 46🥈 41🥉

--- merged/task154.py
+++ merged/task154.py
@@ -5,6 +5,13 @@ p=lambda g,k=59:-k*g or p([*zip(*g[:-(h:=g[v:=k//4].count(2)>4and k%4|1)+v]+g[v-
 p=lambda g:[g:=[*zip(*[g[a-sum(k*(g[a-k*8%15].count(2)>4)for k in b'	')]for a in range(15)])]for _ in g][1]
 p=lambda g,k=29:-k*g or p([*zip(*g[v:=k//2].count(2)>4and g[:v-3]+[g[v-x]for x in b'\r']+g[v+4:]or g)],k-1)
 
+### mwi (110 bytes)
+# alts
+p=lambda g,k=59:-k*g or p([*zip(*g[:-(h:=g[v:=k//4][5:7]==(2,2)and k%4|1)+v]+g[v-h:v-~h][::-1]+g[v-~h:])],k-1)
+##
+p=lambda g,k=59:-k*g or p([*zip(*g[:-(h:=min(g[v:=k//4][5:7])%5and k%4|1)+v]+g[v-h:v-~h][::-1]+g[v-~h:])],k-1)
+p=lambda g,k=59:-k*g or p([*zip(*g[:-(h:={*g[v:=k//4][5:7]}=={2}and k%4|1)+v]+g[v-h:v-~h][::-1]+g[v-~h:])],k-1)
+
 ### joking (114 bytes)
 # this task is (almost) identical to task 390
 p=lambda g,k=-1:k*g or p([*zip(*[g[a+sum(k*2for k in(2,3,-2,-3)if(g*2)[a+k].count(2)>4)]for a in range(15)])],k+1)
@@ -14,9 +21,3 @@ p=lambda g,k=-1:k*g or p([*zip(*[g[~sum(~k*2for k in(1,2,-3,-4)if g[k].count(2)>
 
 ### combined (133 bytes)
 p=lambda i,k=3:-k*i or p([[*[*zip(*i[::-1])][(1<abs(h:=(r:=[*map(max,*i)]).index(2)-l)<4==r.count(2))*2*h+l]]for l in range(15)],k-1)
-
-### mwi (191 (333 unzipped) bytes)
-# alts
-p=lambda g,k=59:-k*g or p([*zip(*g[:-(h:=g[v:=k//4][5:7]==(2,2)and k%4|1)+v]+g[v-h:v-~h][::-1]+g[v-~h:])],k-1)
-p=lambda g,k=59:-k*g or p([*zip(*g[:-(h:=min(g[v:=k//4][5:7])%5and k%4|1)+v]+g[v-h:v-~h][::-1]+g[v-~h:])],k-1)
-p=lambda g,k=59:-k*g or p([*zip(*g[:-(h:={*g[v:=k//4][5:7]}=={2}and k%4|1)+v]+g[v-h:v-~h][::-1]+g[v-~h:])],k-1)
--- mwi/task154.py
+++ mwi/task154.py
@@ -1,4 +1,5 @@
 # alts
 p=lambda g,k=59:-k*g or p([*zip(*g[:-(h:=g[v:=k//4][5:7]==(2,2)and k%4|1)+v]+g[v-h:v-~h][::-1]+g[v-~h:])],k-1)
+##
 p=lambda g,k=59:-k*g or p([*zip(*g[:-(h:=min(g[v:=k//4][5:7])%5and k%4|1)+v]+g[v-h:v-~h][::-1]+g[v-~h:])],k-1)
 p=lambda g,k=59:-k*g or p([*zip(*g[:-(h:={*g[v:=k//4][5:7]}=={2}and k%4|1)+v]+g[v-h:v-~h][::-1]+g[v-~h:])],k-1)
