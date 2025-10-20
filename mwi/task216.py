be6a5ba97dc518c3a73c6247da2b709f613cbff2
H <huntertr2002@gmail.com>
2025-10-20 09:11:07 -0500

    time task216

--- mwi/task216.py
+++ mwi/task216.py
@@ -1,8 +1,8 @@
 # exact copy of ovs/task365.py, just changed grid size
 # way slower though, grid is 20x20 instead of 10x10
 
-# 119 bytes, approx 15 minutes
+# 119 bytes, approx 8 minutes
 exec(f'p=lambda i:max((-{"str(s:=[x[a:b]for x in i[c:d]]).count(%r),"*3}s){"for %s in range(21)"*4})[3]'%(*"021abcd",))
 
-## 121 bytes, approx 8 minutes
+## 121 bytes, approx 6 minutes
 exec(f'p=lambda i:max((-{"sum(s:=[x[a:b]for x in i[c:d]],s).count(%s),"*3}s){"for %s in range(21)"*4})[3]'%(*"021abcd",))
