eab86a6a8d15a7ea430d825fc9236ab4393de11e
ovs <17097105+ovs-code@users.noreply.github.com>
2025-10-22 20:07:57 +0200

    +9=961329/961792 158💎 306🥇 44🥈 40🥉

--- merged/task004.py
+++ merged/task004.py
@@ -1,4 +1,10 @@
-# att (83 vs 74 bytes for gold)
+# ovs (74 bytes, gold)
+import re;p=lambda i:eval(re.sub(r"(\d),(?=.*\1.*].*\1)",r"0,\1|",str(i)))
+
+##
+p=lambda g,s=0:g and[[[v,j][{*g[0]}<={*sum(g[min(2,s:=s+(v>0)):],[])}]for j,v in zip([0]+g[0],g[0])]]+p(g[1:])
+
+### att (83 bytes)
 import re
 p=lambda i:eval(re.sub(r"((.)(, \2)*), 0(?=.*\2.*].*\2)",r"0,\1",str(i)))
 
@@ -8,13 +14,10 @@ import re;p=lambda i:eval(re.sub(r"(((.), )\2*)0,?(?=.*\3.*].*\3)",r"0,\1",str(i
 p=lambda a:[[b.pop(sum([-1,~b[::-1].index(m:=max(b))][:~sum(c)//~m]))]+b for b,c in zip(a,a[1:]+a)]
 p=lambda a:[[b.pop(sum([-1,bytes(b).rfind(m:=max(b))][:~sum(c)//~m]))]+b for b,c in zip(a,a[1:]+a)]
 
-### joking (tied, 83 bytes)
+### joking (83 bytes)
 import re
 p=lambda i:eval(re.sub(r"(([1-9]).*?), 0(?=.*\2.*].*\2)",r"0,\1",str(i)))
 
 ### combined (86 bytes)
 import re
 p=lambda i:eval(re.sub(r"(([^0])(, .)+), 0(?=.*\2.*\].*\2)",r"0,\1",str(i)))
-
-### ovs (110 bytes)
-p=lambda g,s=0:g and[[[v,j][{*g[0]}<={*sum(g[min(2,s:=s+(v>0)):],[])}]for j,v in zip([0]+g[0],g[0])]]+p(g[1:])
