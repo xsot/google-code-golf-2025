6d687404a398d474082ecc85feea91d991ef2116
Jo King <653testing@gmail.com>
2025-10-26 03:09:51 +1100

    +0=961477/961923 163💎 311🥇 47🥈 31🥉

--- joking/task335.py
+++ joking/task335.py
@@ -1,5 +1,6 @@
 p=lambda a,n=10,d=0:~n*a or p([[b.pop()|(n%6*2in b)*(d:=d^4%(sum(c)|4))for c in a[::-1]]for*b,in zip(*a)],n-3)
 
+
 ## almost identical to 246
 p=lambda i,k=1:-k*i or p([*map(lambda*x,b=0:[y|(any(sum(i[b:],[]))*any(sum(i[:(b:=b+1)],[]))>y<k*6+2in x)*4for y in x],*i)],k-1)
 p=lambda a,n=10,d=0:~n*a or p([[b.pop()|(n%6*2in b)*(d:=~9%~sum(c)+d&4)for c in a[::-1]]for*b,in zip(*a)],n-3)
@@ -8,3 +9,6 @@ p=lambda a,n=23,d=0:-n*a or p([[b.pop()|(-~n&10in b)*(d:=d^4%(sum(c)|4))for c in
 p=lambda a,n=24,d=0:n and p([[b.pop()|(n&10in b)*(d:=d^4%(sum(c)|4))for c in a[::-1]]for*b,in zip(*a)],n-6)or a
 p=lambda a,n=25,d=0:1//n*a or p([[b.pop()|(n&10in b)*(d:=d^4%(sum(c)|4))for c in a[::-1]]for*b,in zip(*a)],n-6)
 p=lambda a,n=10,d=0:~n*a or p([[b.pop()|4*(n%6*2in(d:=d^-5%~sum(c)%3)*b)for c in a[::-1]]for*b,in zip(*a)],n-3)
+
+## alt 110
+p=lambda a,d=0:[a:=[[b.pop()|(n in b)*(d:=d^4%(sum(c)|4))for c in a[::-1]]for*b,in zip(*a)]for n in[8,2]*2][3]
--- merged/task331.py
+++ merged/task331.py
@@ -4,13 +4,24 @@ p=lambda i,k=2:k//9*i or p(eval(str([*zip(*i[::-1])]).replace("1,","1,k|")),k*9%
 ### joking (84 bytes)
 p=lambda i,k=7:-k*i or p(eval(str([*zip(*i[::-1])]).replace("1, ","1,86//k%1")),k-2)
 
+##
+p=lambda i:[i:=eval(str([*zip(*i[::-1])]).replace("1, 0","1,"+k))for k in"2786"][3]
+p=lambda i:[i:=eval(str([*zip(*i[::-1])]).replace("1,","1,k|"))for k in[2,7,8,6]][3]
+p=lambda i:[i:=eval(str([*zip(*i[::-1])]).replace("1,",f"1,{k}|"))for k in"2786"][3]
 
 ##
-p=lambda i,*w:i*0!=0and[*map(p,i,i[:1]+i,i[1:]+i[-1:],*w)]or[1,6,7,8,2,0][[i,*w,1].index(1)]
-p=lambda i,*w:i*0!=0and[*map(p,i,i[:1]+i,i[1:]+i[-1:],*w)]or i or[6,7,8,2,0][[*w,1].index(1)]
-p=lambda i,*w:i*0!=0and[*map(p,i,i[:1]+i,i[1:]+i[-1:],*w)]or i or 7%(2-[*w,1].index(1)^4)+5
+p=lambda i,*w:i*0!=0and[*map(p,i,[i]+i,i[1:]+[i],*w)]or[1,6,7,8,2,0][[i,*w,1].index(1)]
+p=lambda i,*w:i*0!=0and[*map(p,i,[i]+i,i[1:]+[i],*w)]or i or[6,7,8,2,0][[*w,1].index(1)]
+p=lambda i,*w:i*0!=0and[*map(p,i,[i]+i,i[1:]+[i],*w)]or i or 7%(2-[*w,1].index(1)^4)+5
+p=lambda i,*w:i*0!=0and[*map(p,i,i[1:]+[i],[i]+i,*w)]or[7,6,2,8,0][[*w,1].index(1)]+i
+p=lambda i,*w:i*0!=0and[*map(p,i,[i]+i,i[1:]+[i],*w)]or b'\0'[[*w,1].index(1)]+i
 
-p=lambda i,*w:i*0!=0and[*map(p,i,i[:1]+i,i[1:]+i[-1:],*w)]or b'\0'[[i,*w,1].index(1)]
+## ties 83
+p=lambda i:[i:=eval(str([*zip(*i[::-1])]).replace("1,","1,k|"))for k in b''][3]
+p=lambda i,*w:i*0!=0and[*map(p,i,[i]+i,i[1:]+[i],*w)]or[6,7,8,2,i][[*w,1].index(1)]
+p=lambda i,*w:i*0!=0and[*map(p,i,i[1:]+[i],[i]+i,*w)]or[7,6,2,8,i][[*w,1].index(1)]
+p=lambda i,*w:i*0!=0and[*map(p,i,[i]+i,i[1:]+[i],*w)]or 7%(2-[*w,1].index(1)^4)+5+i
+p=lambda i,*w:i*0!=0and[*map(p,i,[i]+i,i[1:]+[i],*w)]or 20%(10-[*w,1].index(1)^4)+i
 
 ### combined (86 bytes)
 p=lambda i,k=7:-k*i or p(eval(str([*zip(*i[::-1])]).replace("1, 0","1,86//k%10")),k-2)
