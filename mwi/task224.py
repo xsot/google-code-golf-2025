e077116b249e3683384d67ba732c554a823389e1
H <huntertr2002@gmail.com>
2025-10-26 16:18:53 -0500

    alt

--- mwi/task224.py
+++ mwi/task224.py
@@ -1,1 +1,4 @@
 import re;p=lambda g:[g:=eval(re.sub(" 0(?=,"+a,b,str([*zip(*g)][::-1])))for a,b in[["[0, ]*5?[^5]*$)","0."]]*4+[[" 0.0|, 5)",max({*str(g)}-{*"[5]"})]]*4][7]
+
+##
+import re;p=lambda g:[g:=eval(re.sub(*a,str([*zip(*g)][::-1])))for a in[[" 0(?=,[0, ]*5?[^5]*$)","0."]]*4+[[" 0(?=, 0.0|, 5)",max({*str(g)}-{*"[5]"})]]*4][7]
