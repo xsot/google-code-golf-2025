c0c975a005a6013a6ea69f70dd6ce7c4da7a977f
H <huntertr2002@gmail.com>
2025-10-28 02:23:33 -0500

    +0=961632/962078 164💎 307🥇 55🥈 27🥉

--- merged/task254.py
+++ merged/task254.py
@@ -2,6 +2,8 @@
 p=lambda i,r=[0]*9:[r:=[y or s>0and((x<i[8])is y)*2+1-any(r)for s,y in zip(x,r)]for x in i]
 
 ##
+p=lambda i,r=[0]*9:[[*map(int,r:=[y or((x<i[8])+max(r))*s%3.7for s,y in zip(x,r)]),]for x in i]
+
 def p(i):k=*zip(*i),;return[[y and(s==max(k))+2*(s==sorted({*k})[1])for y,s in zip(x,k)]for x in i]
 
 ### joking (92 bytes)
