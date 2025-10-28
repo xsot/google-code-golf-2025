306e426d358a7e69e89120f6deb9faaf0fd02df4
Jo King <653testing@gmail.com>
2025-10-28 14:58:09 +1100

    +1=961631/962078 164💎 307🥇 54🥈 28🥉

--- joking/task254.py
+++ joking/task254.py
@@ -1,1 +1,6 @@
+# probably can be improved further?
+p=lambda i,r=[0]*9:[r:=[y or s>0and((x<i[-1])is y)*2+1-any(r)for s,y in zip(x,r)]for x in i]
+
+##
 def p(i):k=*zip(*i),;return[[x.pop(0)>>(s!=max(k))+2*(s!=sorted({*k})[1])for s in k]for x in i]
+p=lambda i,r=[]:[r:=[y&3or s>0and((x<i[-1])is y)*2+1-any(r)for s,y in zip(x,r+x)]for x in i]
--- merged/task254.py
+++ merged/task254.py
@@ -1,8 +1,13 @@
-# ovs (93 vs 84 bytes for gold)
-def p(i):k=*zip(*i),;return[[x.pop(0)>>(s<max(k))+2*(s>sorted({*k})[1])for s in k]for x in i]
+# joking (92 vs 84 bytes for gold)
+# probably can be improved further?
+p=lambda i,r=[0]*9:[r:=[y or s>0and((x<i[-1])is y)*2+1-any(r)for s,y in zip(x,r)]for x in i]
 
-### joking (95 bytes)
+##
 def p(i):k=*zip(*i),;return[[x.pop(0)>>(s!=max(k))+2*(s!=sorted({*k})[1])for s in k]for x in i]
+p=lambda i,r=[]:[r:=[y&3or s>0and((x<i[-1])is y)*2+1-any(r)for s,y in zip(x,r+x)]for x in i]
+
+### ovs (93 bytes)
+def p(i):k=*zip(*i),;return[[x.pop(0)>>(s<max(k))+2*(s>sorted({*k})[1])for s in k]for x in i]
 
 ### mwi (99 bytes)
 def p(i):k=*zip(*i),;return[[y and(s==max(k))+2*(s==sorted({*k})[1])for y,s in zip(x,k)]for x in i]
