14b0e2411eb421260ff8bd7056e91bc7b759ad47
ovs <17097105+ovs-code@users.noreply.github.com>
2025-10-30 22:56:53 +0100

    +3=961772/962138 162💎 318🥇 36🥈 38🥉

--- merged/task145.py
+++ merged/task145.py
@@ -1,5 +1,5 @@
-# ovs (164 bytes, gold)
-p=lambda x,k=7,v=2:-k*x or p([[a:=b&2or[b//max(f:=sum(x,[]))+(min({*f}-{2})==b)*8,f.count(b)*4,*[a&~2|b|(v:=v*2)]*6][k]for b in[2]+r][:0:-1]for*r,in zip(*x)],k-1,0)
+# ovs (161 bytes, gold)
+p=lambda x,k=7,v=1:-k*x or p([[a:=b&2or[b//max(f:=sum(x,[]))+(min({*f}-{2})==b)*8,b%511*4,*[a&~2|b|(v:=v*512)]*6][k]for b in[2]+r][:0:-1]for*r,in zip(*x)],k-1,0)
 
 ### joking (170 bytes)
 p=lambda x,k=7,v=2:-k*x or p([[a:=2*(b==2)or(k>1)*(a|(b or(v:=v*2)))or[b//max(f:=sum(x,[]))+min({*f}-{2})//b*8,f.count(b)+2][k]for b in[2]+r][:0:-1]for*r,in zip(*x)],k-1)
