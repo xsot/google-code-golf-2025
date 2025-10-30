75d9ee4a4bf72beeafe0b0e7c857ca2b77b9f33b
H <huntertr2002@gmail.com>
2025-10-29 22:30:30 -0500

    +1=961735/962141 165💎 309🥇 55🥈 26🥉

--- merged/task255.py
+++ merged/task255.py
@@ -1,17 +1,6 @@
-# joking (215 bytes, gold)
-# based on mwi's sol
-import re;p=lambda g,k=15:-k*g or p(eval(re.sub(*[["\((%s|(0, )+3, 3, 3),?"%re.search(r"([ ,03]{61,})(.*\1){3}|$",g:=str([*zip(*g[::-1])]))[1],r"(*[3]*len([\1]),"],["(?=0((,[^,]*){31}|, )[1-9])","-."]][k>9],g)),k-1)
-
+# mwi (214 bytes, gold)
+import re;p=lambda g,k=15:-k*g or p(eval(re.sub(*["\((%s|(0, )+3, 3, 3),?"%re.search(r"([ ,03]{61,})(.*\1){3}|$",g:=str([*zip(*g[::-1])]))[1],"(?=0((,[^,]*){31}|, )[1-9])",r"(*[3]*len([\1]),","-."][k>9::2],g)),k-1)
 ##
-def p(g):
- for y in range(32):g=[[g[y][~x]+13*any({*r[-2%(30-x):31-x]}-{0,3}for r in g[y-1:y+2])for y in range(30)]for x in range(30)];g=[[r[x]%13|3*(len(w:=[r[x]for r in g if{*r[:10]}<={0,3}])>3!={*r[:10]}<={0,3}>={*w}or 3in r[x:]!={*r[:10]}<={0,3})for x in range(30)]for r in g]
- return g
-
-def p(g):
- for y in range(#[*range(32,40,4)]##):g=[[g[y][~x]+#[*range(10,50)]##*any({*r[-2%(30-x):31-x]}-{0,3}for r in g[y-1:y+2])for y in range(30)]for x in range(30)];g=[[r[x]%#[prev_vals[-1]]##|3*(len(w:=[r[x]for r in g if{*r[:10]}<={0,3}])>3!={*r[:10]}<={0,3}>={*w}or 3in r[x:]!={*r[:10]}<={0,3})for x in range(30)]for r in g]
- return g
-
-### mwi (221 bytes)
 import re;p=lambda g,k=15:-k*g or p(eval(re.sub(*[["\((%s|(0, )+3, 3, 3),?"%(re.search(r"([ ,03]{61,})(.*\1){3}",g:=str([*zip(*g[::-1])]))or"__")[1],r"(*[3]*len([\1]),"],["(?=0((,[^,]*){31}|, )[1-9])","-."]][k>9],g)),k-1)
 
 ##
@@ -44,6 +33,19 @@ import re;p=lambda g,k=15:-k*g or p(eval(re.sub(["(?<=\()(%s|(0, )+3, 3, 3),?"%(
 import re;p=lambda g,k=15:-k*g or p(eval(re.sub(["(?<=\()(%s|(0, )+3, 3, 3),?"%(re.search(r"(0(, [03]){20,})(?=(.*\1){3})",g:=str([*zip(*g[::-1])]))or"_")[0],"() 0(?=((,[^,]*){31}|, )[1-9]),"][k>9],"*([3]*len([\\1])or[-.0]),",g)),k-1)
 import re;p=lambda g,k=15:-k*g or p(eval(re.sub(*[["\((%s|(0, )+3, 3, 3),?"%(re.search(r"(0(, [03]){20,})(?=(.*\1){3})",g:=str([*zip(*g[::-1])]))or"_")[0],r"(*[3]*len([\1]),"],["0(?=((,[^,]*){31}|, )[1-9])","-.0"]][k>9],g)),k-1)
 import re;p=lambda g,k=15:-k*g or p(eval(re.sub(*[["\((%s|(0, )+3, 3, 3),?"%(re.search(r"([ ,03]{61,})(?=(.*\1){3})",g:=str([*zip(*g[::-1])]))or"_")[0],r"(*[3]*len([\1]),"],["(?=0((,[^,]*){31}|, )[1-9])","-."]][k>9],g)),k-1)
+
+### joking (215 bytes)
+# based on mwi's sol
+import re;p=lambda g,k=15:-k*g or p(eval(re.sub(*[["\((%s|(0, )+3, 3, 3),?"%re.search(r"([ ,03]{61,})(.*\1){3}|$",g:=str([*zip(*g[::-1])]))[1],r"(*[3]*len([\1]),"],["(?=0((,[^,]*){31}|, )[1-9])","-."]][k>9],g)),k-1)
+
+##
+def p(g):
+ for y in range(32):g=[[g[y][~x]+13*any({*r[-2%(30-x):31-x]}-{0,3}for r in g[y-1:y+2])for y in range(30)]for x in range(30)];g=[[r[x]%13|3*(len(w:=[r[x]for r in g if{*r[:10]}<={0,3}])>3!={*r[:10]}<={0,3}>={*w}or 3in r[x:]!={*r[:10]}<={0,3})for x in range(30)]for r in g]
+ return g
+
+def p(g):
+ for y in range(#[*range(32,40,4)]##):g=[[g[y][~x]+#[*range(10,50)]##*any({*r[-2%(30-x):31-x]}-{0,3}for r in g[y-1:y+2])for y in range(30)]for x in range(30)];g=[[r[x]%#[prev_vals[-1]]##|3*(len(w:=[r[x]for r in g if{*r[:10]}<={0,3}])>3!={*r[:10]}<={0,3}>={*w}or 3in r[x:]!={*r[:10]}<={0,3})for x in range(30)]for r in g]
+ return g
 
 ### ovs (233 (267 unzipped) bytes)
 def p(g):
